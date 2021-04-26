223652c8af1190837b42330f896a52ab2e5edd0e
PENG <2318511681@qq.com>
2021-04-26 21:45:55 +0800

    异步累加新闻阅读量

--- src/main/java/com/peng/news/NewsApplication.java
+++ src/main/java/com/peng/news/NewsApplication.java
@@ -3,6 +3,7 @@ package com.peng.news;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author  PENG
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.peng.news.mapper")
+@EnableAsync
 public class NewsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/peng/news/service/imp/NewsDetailServiceImpl.java
+++ src/main/java/com/peng/news/service/imp/NewsDetailServiceImpl.java
@@ -1,12 +1,14 @@
 package com.peng.news.service.imp;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.peng.news.mapper.NewsMapper;
 import com.peng.news.model.enums.NewsStatus;
 import com.peng.news.model.po.NewsPO;
 import com.peng.news.model.vo.NewsVO;
 import com.peng.news.service.NewsDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 /**
@@ -35,6 +37,25 @@ public class NewsDetailServiceImpl implements NewsDetailService {
             throw new RuntimeException("异常新闻，没有指定新闻栏目或所属栏目不存在或栏目未开启！");
         }
 
+        //增加阅读量
+        increaseReadingCount(newsId);
+
         return newsVO;
+    }
+
+    /**
+     * 增加阅读量
+     * @param newsId
+     */
+    @Async
+    public synchronized void increaseReadingCount(int newsId) {
+
+        //先查询旧值
+        QueryWrapper<NewsPO> queryWrapper = new QueryWrapper<NewsPO>().select("real_reading_count").eq("id", newsId);
+        int oldValue = newsMapper.selectOne(queryWrapper).getRealReadingCount();
+
+        //设置新值
+        UpdateWrapper<NewsPO> updateWrapper = new UpdateWrapper<NewsPO>().set("real_reading_count", oldValue + 1).eq("id", newsId);
+        newsMapper.update(null, updateWrapper);
     }
 }
